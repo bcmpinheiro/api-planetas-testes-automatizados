c3a774c34911204497f2b40c348c0af25c5e0e9f
Barbara Pinheiro <bcmpinheiro@gmail.com>
2023-04-05 21:08:41 -0300

    config testes unitarios na api de planetas

--- src/test/java/com/example/apiplanetastestesautomatizados/common/PlanetaConstantes.java
+++ src/test/java/com/example/apiplanetastestesautomatizados/common/PlanetaConstantes.java
@@ -5,4 +5,5 @@ import com.example.apiplanetastestesautomatizados.domain.Planeta;
 public class PlanetaConstantes {
 
     public static final Planeta PLANETA = new Planeta("name", "clima", "terreno");
+    public static final Planeta PLANETA_INVALIDO = new Planeta(" ", " ", " ");
 }
